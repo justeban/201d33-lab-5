0b40eddd4b2a59ddfd33dce7165f4bd44e333bea
Justin Morris <justinjodymorris@gmail.com>
2018-03-09 16:55:24 -0800

    succesfully created sumArray function for question 4

--- app.js
+++ app.js
@@ -85,13 +85,18 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(testArray) { //eslint-disable-line
-
+function sumArray(testArray) { //eslint-disable-line  
+  var sumOfArray = 0;
+  for (var i = 0; i < testArray.length; i = sum(i,1)[0]) {
+    sumOfArray = sum(sumOfArray, testArray[i])[0];
+  }
+  var sumOfArrayString = testArray.toString() + ' was passed in as an array of numbers, and ' + sumOfArray + ' is their sum.';
+  return [sumOfArray, sumOfArrayString];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -107,6 +112,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(testArray) { //eslint-disable-line
+  var productOfArray = 0;
+  for (var i = 0; i < testArray.length; i = sum(i, 1)[0]) {
+    productOfArray = sum(sumOfArray, testArray[i])[0];
+  }
+
+  var sumOfArrayString = testArray.toString() + ' was passed in as an array of numbers, and ' + sumOfArray + ' is their sum.';
+  return [sumOfArray, sumOfArrayString];
 
 }
 
